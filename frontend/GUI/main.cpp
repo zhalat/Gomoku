dfe44f7c70a90bb0babedbd83ffc574f9c778725
Zbigniew Hałat <zby.halat@gmail.com>
2024-03-17 09:50:50 +0100

    test

--- frontend/GUI/main.cpp
+++ frontend/GUI/main.cpp
@@ -1,19 +1,19 @@
-//#include <QGuiApplication>
-//#include <QQmlApplicationEngine>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
-//    QGuiApplication app(argc, argv);
-//
-//    QQmlApplicationEngine engine;
-//    const QUrl url(u"qrc:/GomokuGui/Main.qml"_qs);
-//    QObject::connect(
-//        &engine,
-//        &QQmlApplicationEngine::objectCreationFailed,
-//        &app,
-//        []() { QCoreApplication::exit(-1); },
-//        Qt::QueuedConnection);
-//    engine.load(url);
-//
-//    return app.exec();
+   QGuiApplication app(argc, argv);
+
+   QQmlApplicationEngine engine;
+   const QUrl url(u"qrc:/GomokuGui/Main.qml"_qs);
+   QObject::connect(
+       &engine,
+       &QQmlApplicationEngine::objectCreationFailed,
+       &app,
+       []() { QCoreApplication::exit(-1); },
+       Qt::QueuedConnection);
+   engine.load(url);
+
+   return app.exec();
 }
