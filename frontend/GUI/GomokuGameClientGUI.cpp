2b6efab2369a83c708f78646cf0b0fe2a69ee7ca
Zbigniew Hałat <zby.halat@gmail.com>
2024-03-17 22:39:13 +0100

    client to test

--- frontend/GUI/GomokuGameClientGUI.cpp
+++ frontend/GUI/GomokuGameClientGUI.cpp
@@ -1,14 +1,6 @@
 #include "Exceptions.h"
 #include "GomokuGameClientGUI.h"
 
-template<class T>
-static ssize_t getMessageSize(const T& message)
-{
-    std::string serializedMsg;
-    message.SerializeToString(&serializedMsg);
-    return serializedMsg.size();
-}
-
 GomokuGameClientGUI::GomokuGameClientGUI(IBoard::Player humanColor)
 :m_humanColor{humanColor}
 {
@@ -26,28 +18,185 @@ IBoard::PositionXY GomokuGameClientGUI::getUserMove() const
     //data
     m_msgQuery.set_k_id(message::MsgID::USR_MOVE_QUERY);
 	if(!m_msgQuery.SerializeToString(&serializedMessage))
-	{
 		throw game_except::General{"Message data query serialization error."};
-	}
 
 	//header
 	m_msgHeader.set_m_msgid(message::MsgID::USR_MOVE_QUERY);
 	m_msgHeader.set_m_msgsize(serializedMessage.size());
 	if(!m_msgHeader.SerializeToString(&serializedHeader))
-	{
 		throw game_except::General{"Message header query serialization error."};
-	}
 
 	IBoard::PositionXY resp;
 	const int stat = sendMsgToServer(serializedHeader, serializedMessage, true, resp);
 
+	if(stat<0)
+		throw game_except::General{"Send data over a socket error."};
+
+
+	return resp;
 }
 
-bool GomokuGameClientGUI::getIsPlayAgain() const{};
-void GomokuGameClientGUI::invalidUserMoveNotify() const{};
-void GomokuGameClientGUI::cpuMoveNotify(IBoard::PositionXY xy) const{};
-void GomokuGameClientGUI::humanMoveNotify(IBoard::PositionXY xy) const{};
-void GomokuGameClientGUI::winnerNotify(IBoard::Player player,vector<IBoard::PositionXY> winnerMark) const{};
-void GomokuGameClientGUI::stalemateNotify() const{};
-void GomokuGameClientGUI::restartGameNotify() const{};
-void GomokuGameClientGUI::endGameNotify() const{};
+bool GomokuGameClientGUI::getIsPlayAgain() const
+{
+
+};
+
+void GomokuGameClientGUI::invalidUserMoveNotify() const
+{
+    std::string serializedHeader;
+    std::string serializedMessage;
+
+    //data
+    m_msgNotify.set_k_id(message::MsgID::USR_MOVE_INVALID_NOTIFY);
+	if(!m_msgNotify.SerializeToString(&serializedMessage))
+		throw game_except::General{"Message data query serialization error."};
+
+	//header
+	m_msgHeader.set_m_msgid(message::MsgID::USR_MOVE_INVALID_NOTIFY);
+	m_msgHeader.set_m_msgsize(serializedMessage.size());
+	if(!m_msgHeader.SerializeToString(&serializedHeader))
+		throw game_except::General{"Message header query serialization error."};
+
+	int none;
+	const int stat = sendMsgToServer(serializedHeader, serializedMessage, false, none);
+
+	if(stat<0)
+		throw game_except::General{"Send data over a socket error."};
+};
+
+void GomokuGameClientGUI::cpuMoveNotify(IBoard::PositionXY xy) const
+{
+    std::string serializedHeader;
+    std::string serializedMessage;
+
+    //data
+    m_msgNotify.set_k_id(message::MsgID::CPU_MOVE_NOTIFY);
+    message::MoveXy* move1 = m_msgNotify.add_m_movies();
+    move1->set_m_x(xy.m_x);
+    move1->set_m_y(xy.m_y);
+	if(!m_msgNotify.SerializeToString(&serializedMessage))
+		throw game_except::General{"Message data query serialization error."};
+
+	//header
+	m_msgHeader.set_m_msgid(message::MsgID::CPU_MOVE_NOTIFY);
+	m_msgHeader.set_m_msgsize(serializedMessage.size());
+	if(!m_msgHeader.SerializeToString(&serializedHeader))
+		throw game_except::General{"Message header query serialization error."};
+
+	int none;
+	const int stat = sendMsgToServer(serializedHeader, serializedMessage, false, none);
+
+	if(stat<0)
+		throw game_except::General{"Send data over a socket error."};
+};
+
+void GomokuGameClientGUI::humanMoveNotify(IBoard::PositionXY xy) const
+{
+
+};
+
+void GomokuGameClientGUI::winnerNotify(IBoard::Player player,vector<IBoard::PositionXY> winnerMark) const
+{
+    std::string serializedHeader;
+    std::string serializedMessage;
+
+    //data
+    m_msgNotify.set_k_id(message::MsgID::WINNER_NOTIFY);
+    message::MoveXy* move1 = m_msgNotify.add_m_movies();
+    message::MoveXy* move2 = m_msgNotify.add_m_movies();
+    message::MoveXy* move3 = m_msgNotify.add_m_movies();
+    message::MoveXy* move4 = m_msgNotify.add_m_movies();
+    message::MoveXy* move5 = m_msgNotify.add_m_movies();
+    move1->set_m_x(winnerMark[0].m_x);
+    move1->set_m_y(winnerMark[0].m_y);
+    move2->set_m_x(winnerMark[1].m_x);
+    move2->set_m_y(winnerMark[1].m_y);
+    move3->set_m_x(winnerMark[2].m_x);
+    move3->set_m_y(winnerMark[2].m_y);
+    move4->set_m_x(winnerMark[3].m_x);
+    move4->set_m_y(winnerMark[3].m_y);
+    move5->set_m_x(winnerMark[4].m_x);
+    move5->set_m_y(winnerMark[4].m_y);
+	if(!m_msgNotify.SerializeToString(&serializedMessage))
+		throw game_except::General{"Message data query serialization error."};
+
+	//header
+	m_msgHeader.set_m_msgid(message::MsgID::WINNER_NOTIFY);
+	m_msgHeader.set_m_msgsize(serializedMessage.size());
+	if(!m_msgHeader.SerializeToString(&serializedHeader))
+		throw game_except::General{"Message header query serialization error."};
+
+	int none;
+	const int stat = sendMsgToServer(serializedHeader, serializedMessage, false, none);
+
+	if(stat<0)
+		throw game_except::General{"Send data over a socket error."};
+};
+
+void GomokuGameClientGUI::stalemateNotify() const
+{
+    std::string serializedHeader;
+    std::string serializedMessage;
+
+    //data
+    m_msgNotify.set_k_id(message::MsgID::STALEMATE_NOTIFY);
+	if(!m_msgNotify.SerializeToString(&serializedMessage))
+		throw game_except::General{"Message data query serialization error."};
+
+	//header
+	m_msgHeader.set_m_msgid(message::MsgID::STALEMATE_NOTIFY);
+	m_msgHeader.set_m_msgsize(serializedMessage.size());
+	if(!m_msgHeader.SerializeToString(&serializedHeader))
+		throw game_except::General{"Message header query serialization error."};
+
+	int none;
+	const int stat = sendMsgToServer(serializedHeader, serializedMessage, false, none);
+
+	if(stat<0)
+		throw game_except::General{"Send data over a socket error."};
+};
+void GomokuGameClientGUI::restartGameNotify() const
+{
+    std::string serializedHeader;
+    std::string serializedMessage;
+
+    //data
+    m_msgNotify.set_k_id(message::MsgID::RESTART_NOTIFY);
+	if(!m_msgNotify.SerializeToString(&serializedMessage))
+		throw game_except::General{"Message data query serialization error."};
+
+	//header
+	m_msgHeader.set_m_msgid(message::MsgID::RESTART_NOTIFY);
+	m_msgHeader.set_m_msgsize(serializedMessage.size());
+	if(!m_msgHeader.SerializeToString(&serializedHeader))
+		throw game_except::General{"Message header query serialization error."};
+
+	int none;
+	const int stat = sendMsgToServer(serializedHeader, serializedMessage, false, none);
+
+	if(stat<0)
+		throw game_except::General{"Send data over a socket error."};
+};
+
+void GomokuGameClientGUI::endGameNotify() const
+{
+    std::string serializedHeader;
+    std::string serializedMessage;
+
+    //data
+    m_msgNotify.set_k_id(message::MsgID::END_NOTIFY);
+	if(!m_msgNotify.SerializeToString(&serializedMessage))
+		throw game_except::General{"Message data query serialization error."};
+
+	//header
+	m_msgHeader.set_m_msgid(message::MsgID::END_NOTIFY);
+	m_msgHeader.set_m_msgsize(serializedMessage.size());
+	if(!m_msgHeader.SerializeToString(&serializedHeader))
+		throw game_except::General{"Message header query serialization error."};
+
+	int none;
+	const int stat = sendMsgToServer(serializedHeader, serializedMessage, false, none);
+
+	if(stat<0)
+		throw game_except::General{"Send data over a socket error."};
+};
--- frontend/GUI/GomokuGameClientGUI.h
+++ frontend/GUI/GomokuGameClientGUI.h
@@ -56,11 +56,13 @@ private:
     //wantResp- set to true if you expect response. False otherwise.
     //resp - container for data server response.
     template<class T>
-    int sendMsgToServer(const std::string header, const std::string data, bool wantResp, T resp) const
+    int sendMsgToServer(const std::string header, const std::string data, bool wantResp, T& resp) const
     {
         int stat{-1};
         int sockData{-1};
+        const auto headSize = getMessageSize(m_msgHeader);
 
+        //first sends header and data to server
         sockData = socket(k_SOCKET_TYPE, SOCK_STREAM, 0);
         stat = connect(sockData, (const struct sockaddr *)&m_sockName, sizeof(m_sockName));
         if (stat == -1)
@@ -73,27 +75,87 @@ private:
 		cout << "Sending..."<< endl;
 
 		ssize_t bytesSent = 0;
-		bytesSent = write(sockData, header.data(), header.size());
-		if(bytesSent != header.size())
+		bytesSent = write(sockData, header.data(), headSize);
+		if(bytesSent != headSize)
 		{
 			close(sockData);
-			return -1;
+			return -2;
 		}
 
 		bytesSent = write(sockData, data.data(), data.size());
 		if(bytesSent != data.size())
 		{
 			close(sockData);
-			return -1;
+			return -3;
 		}
 
+		//we expect response
 		if(wantResp)
 		{
+			//1.read msg header
+			std::string receivedHeaderMessage(headSize,'\0');
+			ssize_t bytesRead = read(sockData, &receivedHeaderMessage[0], headSize);
+			if(bytesRead!=headSize)
+			{
+				close(sockData);
+				return -4;
+			}
 
+	        if(not m_msgHeader.ParseFromString(receivedHeaderMessage))
+	        {
+	        	close(sockData);
+	        	return -5;
+	        }
+
+            const uint32_t length = m_msgHeader.m_msgsize();
+            const uint32_t id = m_msgHeader.m_msgid();
+            if(id >= message::MsgID::KEEP_ME_LAST)
+            {
+            	close(sockData);
+            	return -6;
+            }
+
+	        //2.read msg data
+            std::string receivedMessage(length,'\0');
+            bytesRead = read(sockData, &receivedMessage[0], length);
+			if(bytesRead!=length)
+			{
+				close(sockData);
+				return -7;
+			}
+
+	        if(id==message::MsgID::USR_MOVE_QUERY or
+	           id == message::MsgID::REPLAY_QUERY)
+	        {
+	        	if(m_msgQuery.ParseFromString(receivedMessage))
+	        	{
+	        		cout<<"got query message"<<endl;
+	        	}
+	        }
+	        else if(id == message::MsgID::USR_MOVE_INVALID_NOTIFY or
+	        		id == message::MsgID::CPU_MOVE_NOTIFY or
+					id == message::MsgID::WINNER_NOTIFY or
+					id == message::MsgID::STALEMATE_NOTIFY or
+					id == message::MsgID::RESTART_NOTIFY or
+					id == message::MsgID::END_NOTIFY)
+	        {
+	        	if(m_msgNotify.ParseFromString(receivedMessage))
+	        	{
+	        		cout<<"got notify message"<<endl;
+	        	}
+	        }
 		}
 
-
+		close(sockData);
     	return 0;
+    }
+
+    template<class T>
+    ssize_t getMessageSize(const T& message) const
+    {
+        std::string serializedMsg;
+        message.SerializeToString(&serializedMsg);
+        return serializedMsg.size();
     }
 };
 
